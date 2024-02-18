00df2d5dc533087e2141b03505972dd14c4e219e
Valentin Goldité <valentin.goldite@gmail.com>
2024-02-18 21:19:51 +0100

    ✨ Allow writing in separate file(s) (by default)

--- npdocify/main.py
+++ npdocify/main.py
@@ -1,25 +1,32 @@
 """Main functions for parsing and building docstrings."""
+
 import argparse
 import os
 
 from npdocify.build_doc.main_builder import build_docstring
 from npdocify.difference import Diff
-from npdocify.file_parser import docstring_parse
 from npdocify.lines_routines import add_indent, find_indent
+from npdocify.parse_doc.main_parser import parse_docstring
 
 
-def write_file(file_path: str, docstr_config: dict) -> None:
+def write_file(
+    path: str,
+    out_path: str,
+    *,
+    overwrite: bool,
+    docstr_config: dict,
+) -> None:
     """Write new docstrings on a file."""
-    with open(file_path, encoding="utf-8") as file:
+    with open(path, encoding="utf-8") as file:
         file_lines = file.readlines()
     try:
-        range_docstrs, sections_list = docstring_parse(file_lines)
+        range_docstrs, sections_list = parse_docstring(file_lines)
     except (IndexError, ValueError) as err:
         raise ValueError("Error found during docstring parsing.") from err
     new_lines = []
     for range_doc, sections in zip(range_docstrs, sections_list):
         try:
-            indent_base = find_indent(file_lines[range_doc[0]:range_doc[1]])
+            indent_base = find_indent(file_lines[range_doc[0] : range_doc[1]])
             docstring = build_docstring(
                 sections=sections,
                 docstr_config=docstr_config,
@@ -35,22 +42,36 @@ def write_file(file_path: str, docstr_config: dict) -> None:
     # Write in file
     diff = Diff(range_docstrs, new_lines)
     file_new_lines = diff.apply_diff(file_lines)
-    with open(file_path, "w", encoding="utf-8") as file:
+    out_path = path if overwrite else out_path
+    if os.path.dirname(out_path):
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    with open(out_path, "w", encoding="utf-8") as file:
         file.writelines(file_new_lines)
 
 
 def write_files_recursive(
-    folder_path: str,
+    path: str,
+    out_path: str,
+    *,
+    overwrite: bool,
     docstr_config: dict,
 ) -> None:
     """Write new docstrings on all files in a folder."""
     error_paths = []
-    for dir_path, _, file_names in os.walk(folder_path):
+    for dir_path, _, file_names in os.walk(path):
         for file_name in file_names:
             if file_name.endswith(".py"):
                 file_path = os.path.join(dir_path, file_name)
+                path_clean = path.rstrip(os.sep)
+                out_path_clean = out_path.rstrip(os.sep)
+                file_out_path = file_path.replace(path_clean, out_path_clean)
                 try:
-                    write_file(file_path=file_path, docstr_config=docstr_config)
+                    write_file(
+                        path=file_path,
+                        out_path=file_out_path,
+                        overwrite=overwrite,
+                        docstr_config=docstr_config,
+                    )
                 except (IndexError, ValueError):
                     error_paths.append(file_path)
     if error_paths:
@@ -65,24 +86,30 @@ def parse_args() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="File path or root directory path.", type=str)
     parser.add_argument(
-        "-s",
-        "--style",
-        help="Style of the docstring",
-        type=str,
-        default="numpy"
+        "-s", "--style", help="Style of the docstring", type=str, default="numpy"
     )
     parser.add_argument(
-        "--indent",
-        help="Base indentation size (2, 4, ...)",
-        type=int,
-        default=4
+        "-o",
+        "--out_path",
+        help="Output files dir",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "-w",
+        "--overwrite",
+        help="Whether to overwrite the files or not",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--indent", help="Base indentation size (2, 4, ...)", type=int, default=4
     )
     parser.add_argument(
         "-l",
         "--length",
-        help="Maximum lenght of lines in the docstring.",
+        help="Maximum lenght of lines in the docstring. By default, no limit.",
         type=int,
-        default=88,
+        default=-1,
     )
     args = parser.parse_args()
     docstr_config = {
@@ -90,16 +117,30 @@ def parse_args() -> dict:
         "max_len": args.length,
         "indent": args.indent,
     }
-    return args.path, docstr_config
+    if args.out_path and args.overwrite:
+        raise ValueError(
+            "Cannot use both `--out_path`/`-o` and `--overwrite`/`-w` options."
+        )
+    if not args.out_path and not args.overwrite:
+        raise ValueError(
+            "You must specify an output path with `--out_path`/`-o` "
+            "or overwrite file(s) with `--overwrite`/`-w` instead."
+        )
+    return {
+        "path": args.path,
+        "out_path": args.out_path,
+        "docstr_config": docstr_config,
+        "overwrite": args.overwrite,
+    }
 
 
-def main():
+def main() -> None:
     """Rewrite file(s) docstrings main function."""
-    path, docstr_config = parse_args
-    if os.path.isfile(path):
-        write_file(file_path=path, docstr_config=docstr_config)
-    write_files_recursive(folder_path=path, docstr_config=docstr_config)
+    cli_args = parse_args()
+    if os.path.isfile(cli_args["path"]):
+        write_file(**cli_args)
+    write_files_recursive(**cli_args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
