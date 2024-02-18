b3f801c21191fdff8900e38de82fcc64064f6d77
Valentin Goldité <valentin.goldite@gmail.com>
2024-02-18 21:18:51 +0100

    ✨ Add definition line parser

--- npdocify/file_parser.py
+++ npdocify/file_parser.py
@@ -1,8 +1,9 @@
 """File parsing functions."""
-from typing import List
+
+from typing import List, Tuple
 
 
-def docstring_parse_file_range(lines: List[str]) -> List[List[int]]:
+def parse_ranges(lines: List[str]) -> Tuple[List[List[int]], List[List[int]]]:
     """Parse source code lines ranges.
 
     Parameters
@@ -12,33 +13,88 @@ def docstring_parse_file_range(lines: List[str]) -> List[List[int]]:
 
     Returns
     -------
-    range_docstrs : List[List[int]]
-        List of ranges of docstring line numbers [start, end]. Includes line at start,
-        excludes line at end.
+    ranges_docstr : List[List[int]]
+        Ranges of lines containing docstrings.
+    ranges_def : List[List[int]]
+        Ranges of lines containing function definitions.
     """
-    range_docstrs = []
-    in_docstr = False
+    ranges_docstr: List[List[int]] = []
+    ranges_def: List[List[int]] = []
+    current_docstr_range: List[int] = []
+    current_def_range: List[int] = []
+    in_docstr, in_def = False, False
+    for ind_line, line in enumerate(lines):
+        ranges_docstr, current_docstr_range, in_docstr = docstring_parse_range(
+            ranges=ranges_docstr,
+            current_range=current_docstr_range,
+            in_docstr=in_docstr,
+            line=line,
+            ind_line=ind_line,
+        )
+        if not in_docstr:
+            ranges_def, current_def_range, in_def = def_parse_range(
+                ranges=ranges_def,
+                current_range=current_def_range,
+                in_def=in_def,
+                line=line,
+                ind_line=ind_line,
+            )
+    return ranges_docstr, ranges_def
+
+
+def docstring_parse_range(
+    ranges: List[List[int]],
+    current_range: List[int],
+    *,
+    in_docstr: bool,
+    line: str,
+    ind_line: int,
+) -> Tuple[List[List[int]], List[int], bool]:
+    """Parse docstring lines range from source code."""
     docstring_starters = ('"""', "'''", 'r"""', "r'''")
-    for i, line in enumerate(lines):
-        strip_line = line.strip()
-        if strip_line.startswith(docstring_starters) and line.startswith(" "):
-            # Case one-line docstring
-            if (
-                (strip_line.count('"""') > 1 or strip_line.count("'''") > 1)
-                and not in_docstr
-            ):
-                range_docstrs.append([i, i + 1])
+    strip_line = line.strip()
+    if strip_line.startswith(docstring_starters) and line.startswith(" "):
+        # Case one-line docstring
+        if (
+            strip_line.count('"""') > 1 or strip_line.count("'''") > 1
+        ) and not in_docstr:
+            ranges.append([ind_line, ind_line + 1])
+        else:
+            # Case multi-line docstring
+            if not in_docstr:
+                current_range = [ind_line]
+                in_docstr = True
             else:
-                # Case multi-line docstring
-                if not in_docstr:
-                    current_range = [i]
-                    in_docstr = True
-                else:
-                    current_range.append(i + 1)
-                    range_docstrs.append(current_range)
-                    in_docstr = False
-        elif in_docstr and strip_line.endswith('"""') or strip_line.endswith("'''"):
-            current_range.append(i + 1)
-            range_docstrs.append(current_range)
-            in_docstr = False
-    return range_docstrs
+                current_range.append(ind_line + 1)
+                ranges.append(current_range)
+                in_docstr = False
+    elif in_docstr and strip_line.endswith('"""') or strip_line.endswith("'''"):
+        current_range.append(ind_line + 1)
+        ranges.append(current_range)
+        in_docstr = False
+    return ranges, current_range, in_docstr
+
+
+def def_parse_range(
+    ranges: List[List[int]],
+    current_range: List[int],
+    *,
+    in_def: bool,
+    line: str,
+    ind_line: int,
+) -> Tuple[List[List[int]], List[int], bool]:
+    """Parse docstring lines range from source code."""
+    strip_line = line.rsplit("#", maxsplit=1)[0].strip()
+    if not in_def and strip_line.startswith("def "):
+        if strip_line.endswith(":"):
+            # Case one-line def
+            ranges.append([ind_line, ind_line + 1])
+        else:
+            # Case multi-line def
+            current_range = [ind_line]
+            in_def = True
+    elif in_def and strip_line.endswith(":"):
+        current_range.append(ind_line + 1)
+        ranges.append(current_range)
+        in_def = False
+    return ranges, current_range, in_def
