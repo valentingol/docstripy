2c74d14187fef609f377e06f8fb16c55fafda20a
Valentin Goldité <valentin.goldite@gmail.com>
2024-02-18 21:20:55 +0100

    ✨ Limit the length of the title if needed

---
+++ npdocify/build_doc/preprocessing.py
@@ -0,0 +1,10 @@
+"""Preprocessing functions for some sections."""
+
+from typing import List
+
+from npdocify.line_break import line_break
+
+
+def preprocess_title_build(lines: List[str], max_len: int) -> List[str]:
+    """Post-process the title section."""
+    return line_break(lines, max_line_length=max_len)
