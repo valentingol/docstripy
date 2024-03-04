eaa461c432556980c432aadf1f7f5bb00aad4593
valentingol <valentin.goldite@gmail.com>
2024-03-04 19:15:28 +0100

    ✏️ Adapt line number to file changes

--- tests/test_parse_params.py
+++ tests/test_parse_params.py
@@ -10,11 +10,11 @@ def test_params() -> None:
     """Test params parsing."""
     with open("tests/files/test3.py", "r", encoding="utf-8") as file:
         lines = file.readlines()
-    rest_doc1 = remove_indent(lines[18:29])
+    rest_doc1 = remove_indent(lines[19:30])
     params_rest = parse_params_all(rest_doc1, style="rest")
-    np_doc1 = remove_indent(lines[55:68])
+    np_doc1 = remove_indent(lines[56:69])
     params_np = parse_params_all(np_doc1, style="numpy")
-    google_doc1 = remove_indent(lines[93:102])
+    google_doc1 = remove_indent(lines[94:103])
     params_google = parse_params_all(google_doc1, style="google")
     expected_list = [
         {
@@ -48,11 +48,11 @@ def test_params() -> None:
     check.equal(params_np, expected_list)
     check.equal(params_google, expected_list)
 
-    rest_doc2 = remove_indent(lines[40:41])
+    rest_doc2 = remove_indent(lines[41:42])
     params_dict_rest2 = parse_params_all(rest_doc2, style="rest")
-    np_doc2 = remove_indent(lines[81:84])
+    np_doc2 = remove_indent(lines[82:85])
     params_dict_np2 = parse_params_all(np_doc2, style="numpy")
-    google_doc2 = remove_indent(lines[113:115])
+    google_doc2 = remove_indent(lines[114:116])
     params_dict_google2 = parse_params_all(google_doc2, style="google")
     expected_dict = [
         {"name": "a", "type": "", "optional": False, "description": [], "default": ""},
