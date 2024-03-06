295593df9717a5e7fdbf8482eb8ad46e97545d03
valentingol <valentin.goldite@gmail.com>
2024-03-06 17:31:25 +0100

    ✅ Add test support file for class docstring

---
+++ tests/files/class.py
@@ -0,0 +1,20 @@
+"""A test module.
+
+Contains some classes for testing the parser.
+"""
+
+
+class MyClass:
+    """My class.
+
+    Parameters
+    ----------
+    attr:
+        The attribute
+    """
+
+    # A comment
+    cls_attr: 0  # some class attribute
+
+    def __init(self, attr: int = 0) -> None:
+        self.attr = attr
