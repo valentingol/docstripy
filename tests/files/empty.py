d20ac0ef213ef277387eb11a9e5a80229fd333db
valentingol <valentin.goldite@gmail.com>
2024-03-06 17:31:39 +0100

    ✅ Add functions without docstrings

--- tests/files/empty.py
+++ tests/files/empty.py
@@ -7,3 +7,15 @@ Contains some functions with no docstrings.
 
 def clean_trailing_spaces() -> None:  # noqa: D103
     pass
+
+
+def add_indentation_spaces() -> None:  # noqa: D103
+    pass
+
+
+def remove_end_of_line() -> None:  # noqa: D103
+    pass
+
+
+def add_end_of_line() -> None:  # noqa: D103
+    pass
