cf135a49eaf371ff696da02ec9af33baf8a3c612
valentingol <valentin.goldite@gmail.com>
2024-03-07 14:34:30 +0100

    ✅ Cover jupyter notebooks

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,11 +4,12 @@ import os
 import shutil
 import sys
 
+import nbformat
 import pytest
 import pytest_check as check
 
 from docstripy.main import main, parse_args
-from docstripy.write import write_file, write_files_recursive
+from docstripy.write import write_file_ipynb, write_file_py, write_files_recursive
 
 
 def test_main() -> None:
@@ -46,6 +47,9 @@ def test_main() -> None:
         check.is_true(os.path.exists(path), f"File not found: {path}")
         path = f"tests/tmp/{style}/class.py"
         check.is_true(os.path.exists(path), f"File not found: {path}")
+        path = f"tests/tmp/{style}/notebook.ipynb"
+        check.is_true(os.path.exists(path), f"File not found: {path}")
+    # Check no docstring
     with open("tests/tmp/google/empty.py", encoding="utf-8") as file:
         lines = file.readlines()
     check.equal(lines[8], '    """Clean trailing spaces."""\n')
@@ -57,6 +61,7 @@ def test_main() -> None:
     )
     check.equal(run, 0)
     check.is_true(os.path.exists("tests/tmp/class.py"))
+    # Test class docstring
     with open("tests/tmp/class.py", encoding="utf-8") as file:
         lines = file.readlines()
     check.equal("    attr : int, optional\n", lines[12])
@@ -64,6 +69,18 @@ def test_main() -> None:
     check.equal("    attr : int\n", lines[28])
     check.equal('    """\n', lines[30])
     check.equal('        """Forward."""\n', lines[33])
+    # Test notebooks
+    run = os.system(
+        "docstripy tests/files/notebook.ipynb -o tests/tmp/notebook.ipynb "
+        "-s numpy --len 88 --indent 4"
+    )
+    check.is_true(os.path.exists("tests/tmp/notebook.ipynb"))
+    with open("tests/tmp/notebook.ipynb", encoding="utf-8") as file:
+        file_dict = nbformat.read(file, as_version=nbformat.NO_CONVERT)
+    cell0 = file_dict["cells"][0]["source"].split("\n")
+    check.equal("    x : int, optional", cell0[5])
+    cell1 = file_dict["cells"][1]["source"].split("\n")
+    check.equal("    x : str, optional", cell1[5])
     if os.path.exists("tests/tmp"):
         shutil.rmtree("tests/tmp")
     sys.argv = old_argv
@@ -71,23 +88,44 @@ def test_main() -> None:
 
 def test_errors(capfd: pytest.CaptureFixture) -> None:
     """Test entry point when there are files errors."""
+    old_argv = sys.argv.copy()
+    # Case error in parsing
     with pytest.raises(ValueError, match="Error found during docstring parsing."):
-        write_file(
+        write_file_py(
             "tests/wrong_files/file1.py",
             "tests/tmp/file1.py",
             overwrite=False,
             docstr_config={"style": "numpy", "max_len": 88, "indent": 2},
         )
+    # Case error in building
     for style in ("numpy", "rest", "google"):
         with pytest.raises(
             ValueError, match="Error found at lines 3-8 during docstring building.*"
         ):
-            write_file(
+            write_file_py(
                 "tests/wrong_files/file2.py",
                 "tests/tmp/file2.py",
                 overwrite=False,
                 docstr_config={"style": style, "max_len": 88, "indent": 2},
             )
+    # case wrong extension
+    with pytest.raises(ValueError, match="Output file must be a .py file.*"):
+        write_file_py(
+            "tests/files/empty.py",
+            "tests/tmp/empty.ipynb",
+            overwrite=False,
+            docstr_config={"style": "numpy", "max_len": 88, "indent": 2},
+        )
+    with pytest.raises(ValueError, match="Output file must be a .ipynb file.*"):
+        write_file_ipynb(
+            "tests/files/notebook.ipynb",
+            "tests/tmp/notebook.py",
+            overwrite=False,
+            docstr_config={"style": "numpy", "max_len": 88, "indent": 2},
+        )
+    sys.argv = ["docstripy", "tests/files/docstr1.txt", "-o", "tests/tmp/docstr1.txt"]
+    with pytest.raises(ValueError, match="File extension not supported.*"):
+        main()
     # Case multiple files
     write_files_recursive(
         "tests/wrong_files",
@@ -98,11 +136,10 @@ def test_errors(capfd: pytest.CaptureFixture) -> None:
     out, _ = capfd.readouterr()
     check.is_true(out.startswith("Error when parsing file(s):\n"))
     # Case args errors
-    old_argv = sys.argv.copy()
-    sys.argv = ["docstripy", ".", "-o", "./out", "-w"]
+    sys.argv = ["docstripy", "tests/files", "-o", "tests/tmp", "-w"]
     with pytest.raises(ValueError, match="Cannot use both.*"):
         parse_args()
-    sys.argv = ["docstripy", "."]
+    sys.argv = ["docstripy", "tests/files"]
     with pytest.raises(ValueError, match="You must specify an output path.*"):
         parse_args()
     sys.argv = old_argv
