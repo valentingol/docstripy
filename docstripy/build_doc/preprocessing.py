da9d1caeaec2d9614d8e66ab3f33283011fe8106
Valentin Goldité <valentin.goldite@gmail.com>
2024-06-29 22:36:34 +0200

    🚀 Separe the first line (short title) from description

--- docstripy/build_doc/preprocessing.py
+++ docstripy/build_doc/preprocessing.py
@@ -3,8 +3,11 @@
 from typing import List
 
 from docstripy.line_break import line_break
+from docstripy.lines_routines import split_first_line
 
 
 def preprocess_title_build(lines: List[str], max_len: int) -> List[str]:
     """Post-process the title section."""
-    return line_break(lines, max_line_length=max_len)
+    lines = split_first_line(lines, max_line_length=max_len)
+    lines = line_break(lines, max_line_length=max_len)
+    return lines
