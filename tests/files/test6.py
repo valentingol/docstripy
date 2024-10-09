a55073ddca6138b47b78d5a5ccd8c8c86c7c9d9f
valentingol <valentin.goldite@gmail.com>
2024-10-09 11:02:12 +0200

    ✅ Cover bug fix with dot in default value

---
+++ tests/files/test6.py
@@ -0,0 +1,14 @@
+"""A test module.
+
+Contains some functions for testing the parser.
+"""
+
+
+def my_func(alpha: float = 0.25) -> float:
+    """Perform a computation.
+
+    Args:
+        alpha (float, optional): A parameter.
+            Defaults to 0.25.
+    """
+    return alpha**2
